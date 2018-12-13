8cc940b2d63f0e7c4864290349c469158208018d
s-tamiya <sy.tamiya@gmail.com>
2018-12-13 16:46:13 +0900

    First Commit

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ sample
 ======
 
 A Symfony project created on December 10, 2018, 6:47 am.
+
+実行コマンド
+cd C:\xampp\htdocs\sample
+php bin/console server:run
---
+++ src/AppBundle/Controller/LuckyController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class LuckyController
+{
+  /**
+  * @Route("/lucky/number")
+  */
+  public function numberAction()
+  {
+    $number = rand(0, 100);
+
+    return new Response(
+        '<html><body>Lucky Number: '.$number.'</body></html>'
+      );
+  }
+
+  /**
+  * @Route("/api/lucky/number")
+  */
+  public function apiNumberAction()
+  {
+    $data = array(
+      'lucky_number' => rand(0, 100),
+    );
+
+  return new Response(
+      json_encode($data),
+      200,
+      array('Context-Type' => 'application/json')
+    );
+  }
+
+  /**
+  * @Route("/lucky/number/{$count}")
+  */
+  public function numberCountAction($count)
+  {
+      $numbers = array();
+      for ($i = 0; $i < $count; $i++) {
+        $numbers[$i] = rend(0, 100);
+      }
+      $numbersList = implode(', ', $numbers);
+
+      return new Response(
+        '<html><body>Lucky Number: '.$numbersList.'</body></html>'
+      );
+  }
+}
